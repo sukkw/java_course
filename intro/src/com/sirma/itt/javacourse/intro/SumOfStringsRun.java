a61356af7878390391721461920fcb28fcac8710
stella <sdjulgerova@gmail.com>
2013-04-20 22:57:46 +0200

    updates

--- intro/src/com/sirma/itt/javacourse/intro/SumOfStringsRun.java
+++ intro/src/com/sirma/itt/javacourse/intro/SumOfStringsRun.java
@@ -16,6 +16,6 @@ public class SumOfStringsRun {
 	 */
 	public static void main(String[] args) {
 		SumOfStrings sum = new SumOfStrings();
-		System.out.println(sum.calculate("234", "77"));
+		System.out.println(sum.calculate("238", "77"));
 	}
 }
