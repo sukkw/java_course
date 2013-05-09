816b8fe602e131f064da1d44478c999946d4b651
sdjulgerova <sdjulgerova@gmail.com>
2013-05-09 14:46:36 +0300

    initial version

--- collections/src/main/java/com/sirma/itt/javacourse/collections/PageBean.java
+++ collections/src/main/java/com/sirma/itt/javacourse/collections/PageBean.java
@@ -1,18 +1,139 @@
 package com.sirma.itt.javacourse.collections;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Get list with text lines. Separate text in pages and print 
+ * different page depending on user choice.
+ * 
+ * @version 1.1 09 May 2013
+ * @author Stella Djulgerova
+ */
 public class PageBean {
-/*
-	private next() – връща следващите няколко елемента от списъка;
-	privateprevious() – връща предишните няколко елемента на списъка;
-	
-	private boolean hasNext(){
-		
+
+	// class private members
+	private int pageSize;
+	private List<String> textLines = new ArrayList<String>();
+	private int currentPage;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param pageSize - number of elements to be shown
+	 * @param textLines - list with text lines
+	 */
+	public PageBean(int pageSize, List<String> textLines) {
+		this.pageSize = pageSize;
+		this.textLines = textLines;
 	}
-	private boolean hasPrevious(){
-		
+
+	/**
+	 * Calculate number of all available pages.
+	 * @return - number of all pages
+	 */
+	public int pagesCount() {
+
+		int pagesCount;
+		if (textLines.size() % pageSize == 0) {
+			pagesCount = textLines.size() / pageSize;
+		} else {
+			pagesCount = (textLines.size() / pageSize) + 1;
+		}
+
+		return pagesCount;
 	}
-	
-	firstPage() – връща първата страница и я прави текуща;
-	lastPage() – връща последната страница и я прави текуща;
-	getCurrentPageNumber() – връща номера на текущата страница.*/
+
+	/**
+	 * Print element from next page on screen.
+	 */
+	public void next() {
+		
+		currentPage = getCurrentPageNumber() + 1;
+		int startPosition = getCurrentPageNumber() * pageSize;
+		int endPosition = (getCurrentPageNumber() * pageSize) + pageSize;
+		
+		if(endPosition > textLines.size() ) {
+			endPosition = textLines.size();
+			currentPage = getCurrentPageNumber() - 1;
+		}
+		for(int i=startPosition; i<endPosition; i++) {
+			System.out.println(textLines.get(i));
+		}
+	}
+
+	/**
+	 * Print elements from previous page on screen.
+	 */
+	public void previous() {
+
+		if(hasPrevious()) {
+			currentPage = getCurrentPageNumber() - 1;
+			int startPosition = getCurrentPageNumber() * pageSize;
+			int endPosition = (getCurrentPageNumber() * pageSize) + pageSize;
+			
+			for(int i=startPosition; i<endPosition; i++) {
+				System.out.println(textLines.get(i));
+			}
+		} else {
+			System.out.println("Can not navigate to previous page!");
+		}
+	}
+
+	/**
+	 * Check if next page is available.
+	 * 
+	 * @return true if there's next page or false if there's no next page
+	 */
+	public boolean hasNext() {
+
+		if (getCurrentPageNumber() < pagesCount()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Check if previous page is available.
+	 * 
+	 * @return true if there's previous page or false if there's no previous page
+	 */
+	public boolean hasPrevious() {
+		if (getCurrentPageNumber() > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Get all elements from first page and print it on the screen.
+	 */
+	public void firstPage() {
+		currentPage = 0;
+		for(int i=0; i<pageSize; i++) {
+			System.out.println(textLines.get(i));
+		}
+	}
+
+	/**
+	 * Get all elements from last page and print it on the screen.
+	 */
+	public void lastPage() {
+		int startPosition = (pagesCount()-1) * pageSize;
+		for(int i=startPosition; i<textLines.size(); i++) {
+			System.out.println(textLines.get(i));
+		}
+		currentPage = pagesCount()-1;
+	}
+
+	/**
+	 * Get number of the current page shown on screen.
+	 * @return number of the page
+	 */
+	public int getCurrentPageNumber() {
+		return currentPage;
+	}
+
 }
--- collections/src/main/java/com/sirma/itt/javacourse/collections/PageBeanRun.java
+++ collections/src/main/java/com/sirma/itt/javacourse/collections/PageBeanRun.java
@@ -1,10 +1,68 @@
 package com.sirma.itt.javacourse.collections;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * PageBean run class. Get text from file and put lines in array list. 
+ * Allow user to chose different actions for text visualization in pages.
+ * 
+ * @version 1.1 09 May 2013
+ * @author Stella Djulgerova
+ */
 public class PageBeanRun {
 
-	public static void main() {
-		PageBean pages = new PageBean();
+	public static void main(String[] args) {
+
+		// read from text file and put lines into list
+		Path path = Paths.get("info.txt");
+		List<String> lines = new ArrayList<String>();
+		String line = null;
+		String repeat;
+		Scanner input = new Scanner(System.in);
 		
+		try (BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
+
+			while ((line = reader.readLine()) != null) {
+				lines.add(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		PageBean pages = new PageBean(10, lines);
+		System.out.println(pages.pagesCount());
+		pages.firstPage();
 		
+		do {
+			System.out.println(" 1-First / 2-Next / 3-Previous / 4-Last / 5-Exit");
+			repeat = input.next();
+			
+			switch (repeat) {
+
+				case "1":
+					pages.firstPage();
+					break;
+				case "2":
+					pages.next();
+					break;
+				case "3":
+					pages.previous();
+					break;
+				case "4":
+					pages.lastPage();
+					break;
+				default:
+					break;
+			}
+
+		} while (!"5".equals(repeat));
 	}
 }
