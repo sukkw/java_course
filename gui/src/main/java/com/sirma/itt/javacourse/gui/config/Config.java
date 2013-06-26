f59bbfa40ab8a129a5579fa51dd74c4f652bafff
sdjulgerova <sdjulgerova@gmail.com>
2013-06-26 16:53:17 +0300

    Updates

---
+++ gui/src/main/java/com/sirma/itt/javacourse/gui/config/Config.java
@@ -0,0 +1,12 @@
+package com.sirma.itt.javacourse.gui.config;
+
+public class Config {
+	public static final int MIN_PORT = 7000;
+	public static final int MAX_PORT = 7020;
+	public static final String HOST = "localhost";
+	public static final int BUF_SIZE = 1024;
+	public static final int WAITING_TIME = 1000;
+	
+	public static final String CHANNEL1 = "235.0.0.1";
+	public static final String CHANNEL2 = "235.255.0.1";	
+}
