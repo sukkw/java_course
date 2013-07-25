4452e4621b867532930afab46473bfcdeb2ab1c3
sdjulgerova <sdjulgerova@gmail.com>
2013-07-25 17:51:08 +0300

    Updates and bug fixes

--- chat/src/main/java/com/sirma/itt/javacourse/servermain/ServerStartClass.java
+++ chat/src/main/java/com/sirma/itt/javacourse/servermain/ServerStartClass.java
@@ -1,8 +1,21 @@
 package com.sirma.itt.javacourse.servermain;
 
+import com.sirma.itt.javacourse.server.ServerMainThread;
+
+/**
+ * Server run class. Starts server.
+ * 
+ * @version 1.1 03	July 2013
+ * @author Stella Djulgerova
+ */
 public class ServerStartClass {
-	public static void main(String[] args) {
 	
+	/**
+	 * Class main method
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new ServerMainThread();
 	}
 }
 
